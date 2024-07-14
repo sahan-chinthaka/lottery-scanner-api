ebdedbbf5a29c9c362b698a80b043165810a0c62
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-07-14 16:54:49 +0530

    Connect firebase firestore

--- api/app.py
+++ api/app.py
@@ -1,5 +1,20 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("./api/smart-lottery-adminsdk.json")
+firebase_admin.initialize_app(cred,)
+
+store = firestore.client()
+col_ref = store.collection("users")
+
+
 def main():
-    print("Main")
+    doc_ref = col_ref.document()
+    doc_ref.set(
+        {
+            "name": "Test",
+        },
+    )
 
 
 if __name__ == "__main__":
