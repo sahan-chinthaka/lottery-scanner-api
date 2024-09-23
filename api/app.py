edf7364a76078a5d917be48dd89a5963e6523c02
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-09-23 13:29:24 +0530

    Create a test endpoint for image scanning

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from scrapper import get_dlb_results, get_nlb_results
 
 app = Flask(__name__)
@@ -15,6 +15,13 @@ def getLatestResults():
     nlb = get_nlb_results()
 
     return jsonify(nlb + dlb)
+
+
+@app.post("/scan-image")
+def scanImage():
+    print(dir(request.files))
+
+    return jsonify({"msg": "done"})
 
 
 def main(d=True):
