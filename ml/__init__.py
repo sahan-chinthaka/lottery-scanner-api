4e066c738fce838d2fbebea23bdbc33a987451b5
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-09-23 15:02:51 +0530

    Make lottery type predictor available for other modules

---
+++ ml/__init__.py
@@ -0,0 +1,6 @@
+from .predict_lottery_type import get_lottery_type
+from .text_extract import (
+    extract_koti_kapruka,
+    extract_govisetha,
+    extract_mahajana_sampatha,
+)
--- ml/predict_lottery_type.py
+++ ml/predict_lottery_type.py
@@ -1,28 +1,6 @@
 import torch
 import torch.nn as nn
 from torchvision import models, transforms
-from PIL import Image
-
-
-def predict_lottery_type(frame, model, transform, class_names):
-    # image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    image = frame
-
-    image = transform(image).unsqueeze(0)
-    image = image.to(device)
-
-    model.eval()
-
-    with torch.no_grad():
-        outputs = model(image)
-        print(outputs)
-
-        _, predicted = torch.max(outputs, 1)
-
-        predicted_class = class_names[predicted.item()]
-
-    return predicted_class
-
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -30,7 +8,7 @@ model = models.resnet18(pretrained=False)
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 3)
 
-model.load_state_dict(torch.load("./model/v1-epoch-2.pth"))
+model.load_state_dict(torch.load("./ml/model/v1-epoch-2.pth"))
 model = model.to(device)
 
 transform = transforms.Compose(
@@ -43,8 +21,24 @@ transform = transforms.Compose(
 
 class_names = ["Govisetha", "Koti Kapurka", "Mahajana Sampatha"]
 
-predicted_class = predict_lottery_type(
-    Image.open("./test.jpg"), model, transform, class_names
-)
 
-print(predicted_class)
+def predict_lottery_type(frame, model, transform, class_names):
+    image = frame
+
+    image = transform(image).unsqueeze(0)
+    image = image.to(device)
+
+    model.eval()
+
+    with torch.no_grad():
+        outputs = model(image)
+
+        _, predicted = torch.max(outputs, 1)
+
+        predicted_class = class_names[predicted.item()]
+
+    return predicted_class
+
+
+def get_lottery_type(image):
+    return predict_lottery_type(image, model, transform, class_names)
